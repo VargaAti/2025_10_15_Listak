222095fde541a98eb155fa349cac8d0b13e54d10
VargaAti <varga.attila@diak.szbi-pg.hu>
2025-10-15 10:42:09 +0200

    Update 2.py

--- feladatok/2.py
+++ feladatok/2.py
@@ -6,11 +6,10 @@ import random as r
 numbers = []
 for i in range(10):
         numbers.append(r.randint(1,3))
-        i += 1
 print(f"Lista: {numbers}")
 num_to_del = int(input("Adj meg egy számot amit törölni szeretnél a listából: "))
 # filtered = list(filter(lambda x: x != num_to_del, numbers))
 # print(f"Lista: {filtered}")
 while num_to_del in numbers:
-        print(numbers.remove(num_to_del))
+        numbers.remove(num_to_del)
 print(numbers)
